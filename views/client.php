272ff8cdb4fcc16ed8b63241887be64e7e2d3981
oumaima <oumaimaabouelhaitam@gmail.com>
2022-06-21 21:22:01 +0100

    update

--- views/client.php
+++ views/client.php
@@ -50,7 +50,6 @@
                                 <td class="Phone"><?= $admine['Phone'] ?></td>
                                 <td class="adres"><?= $admine['adresse'] ?></td>
                                 <td class="d-flex  align-items-start">
-                                <a href="#" class="btn btn-outline-primary btn-lg fw-bold update" style="  color:primary;" data-bs-toggle="modal" data-bs-target="#myModel"><img src="https://img.icons8.com/fluency/20/000000/edit-user-female.png" /></a>
                                 <form action="operation" method="POST" class="confirm-submit" data-confirm-msg="etes vous sure de vouloir continuez ?" >
                                     <button type="submit" name ="deleclient" class="btn btn-outline-danger" 
                                     
@@ -64,67 +63,6 @@
 
      
      
-        <div class="col-sm6 mt-3 " style="float: right;">
-            <div class="modal fade" id="myModel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" style="max-width: 800px;">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Update  </h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body container">
-                         <form class="form-container row" action="operation" method="POST" data-parsley-validate>   
-                                  <div class="mb-3 fw-bold" >
-                                       <input type="text" hidden class="form-control" id="id" name="id"  >
-                                       
-                                   </div>
-                               
-                                      <div class="mb-3 fw-bold col-6" >
-                                            <label for="exampleFormControlInput1" class="form-label">Nom complet</label>
-                                            <input type="text" class="form-control"  id ="name"  name="nom"  placeholder="Enter name complet" >
-                                        </div>
-                                        <div class="mb-3 fw-bold col-6" >
-                                            <label for="exampleFormControlInput1" class="form-label">cin</label>
-                                            <input type="text" class="form-control"  id ="cin"  name="cin"  placeholder="Enter name complet" >
-                                        </div>
-                                        <div class="mb-3  fw-bold col-6"  >
-                                            <label for="exampleFormControlInput1" class="form-label">Genre</label>
-                                            <select class="form-control" name="genre" id="genre" >
-                                             <option value="Femme">Femme </option>
-                                             <option value="Homme" selected>Homme </option>
-                                         </select>
-                                        </div>
-                                       
-                                        <div class="mb-3  fw-bold col-6"  >
-                                            <label for="exampleFormControlInput1" class="form-label">Email</label>
-                                            <input type="email" class="form-control" name="email" id="email" placeholder="Enter votre email">
-                                        </div>
-                                
-                                        <div class="mb-3  fw-bold col-6"  >
-                                            <label for="exampleFormControlInput1" class="form-label">password</label>
-                                            <input type="text" class="form-control" name="password" id ="password" placeholder="Enter votre password">
-                                          
-                                        </div>
-                                        <div class="mb-3  fw-bold col-6"  >
-                                            <label for="exampleFormControlInput1" class="form-label">Phone</label>
-                                            <input type="text" class="form-control" id="phone"  name="Phone" placeholder="Enter phone">                                      
-                                        </div>
-                                        <div class="mb-3  fw-bold col-6" >
-                                            <label for="exampleFormControlTextarea1" class="form-label">Address</label>
-                                            <textarea class="form-control"  name="adres" id="adress" rows="3"></textarea>
-                                        </div>
-             
-                                    <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                            <button type="submit" name="update" class="btn btn-warning mT-3">Update</button>
-                                    </div>
-                                </form>              
-                        </div>           
-                        </div>
-                        <div class="modal-footer">
-                      
-                    </div>
-                </div>
             </div>
             <script src="views/assets/bootstrap.bundle.min.js" ></script>
             <script src="views/assets/js/bootstrap.bundle.min.js" ></script>
